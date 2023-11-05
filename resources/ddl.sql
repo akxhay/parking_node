0602231b7a466519d0c98a53f12c10c5e5c49d70
a13400273 <akshay.sharma@africa.airtel.com>
2023-11-05 15:07:21 +0530

    updated ddl file

--- resources/ddl.sql
+++ resources/ddl.sql
@@ -1,45 +1,37 @@
-CREATE TABLE public.parking_lots
-(
-  id bigserial NOT NULL,
-"name" varchar
-(
-255
-)
-NOT NULL,
-CONSTRAINT parking_lots_name_key null,
-CONSTRAINT parking_lots_pkey null
-);
+create table
+    public.parking_lots (
+        id bigserial not null,
+        "name" varchar (255) not null,
+        constraint parking_lots_name_key null,
+        constraint parking_lots_pkey null
+    );
 
-CREATE TABLE public.floors
-(
-  id             bigserial NOT NULL,
-  parking_lot_id int8 NOT NULL,
-"name" varchar
-(
-255
-)
-NOT NULL,
-CONSTRAINT floors_pkey null
-);
+create table
+    public.floors (
+        id bigserial not null,
+        parking_lot_id int8 not null,
+        "name" varchar (255) not null,
+        constraint floors_pkey null
+    );
 
+alter table public.floors
+add
+    constraint fk9o9d21ndox2oqaqo70maj88ao foreign key(parking_lot_id) references public.parking_lots(id);
 
-ALTER TABLE public.floors ADD CONSTRAINT fk9o9d21ndox2oqaqo70maj88ao FOREIGN KEY(parking_lot_id)REFERENCES public.
-parking_lots(id);
+create table
+    public.parking_slots (
+        is_occupied bool not null,
+        slot_number int4 not null,
+        arrived_at int8 null,
+        floor_id int8 not null,
+        id bigserial not null,
+        number_plate varchar(255) null,
+        slot_type varchar(255) not null,
+        constraint parking_slots_number_plate_key null,
+        constraint parking_slots_pkey null
+    );
 
-
-CREATE TABLE public.parking_slots
-(
-  is_occupied  bool NOT NULL,
-  slot_number  int4 NOT NULL,
-  arrived_at   int8 NULL,
-  floor_id     int8 NOT NULL,
-  id           bigserial NOT NULL,
-  number_plate varchar(255) NULL,
-  slot_type    varchar(255) NOT NULL,
-CONSTRAINT parking_slots_number_plate_key null,
-CONSTRAINT parking_slots_pkey null
-);
-
-
-ALTER TABLE public.parking_slots ADD CONSTRAINT fklwqj0vlq3y26h4ikgaboijwiq FOREIGN KEY(floor_id)REFERENCES public.
-floors(id);
+alter table
+    public.parking_slots
+add
+    constraint fklwqj0vlq3y26h4ikgaboijwiq foreign key(floor_id) references public.floors(id);
